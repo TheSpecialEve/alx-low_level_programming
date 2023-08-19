c185f220e990cceea82ac07115065fb315a535a6
Evelyn <evelyn7d7@gmail.com>
2023-08-18 18:27:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - print 1 to 100 and fizzbuzz
+ *
+ * Rwturn: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+		printf("fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("fizz");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("buzz");
+		}
+		else
+		{
+		printf("%d", i);
+		}
+
+	}	
+	printf("buzz");
+	printf("\n");
+
+	return (0);
+}
+
+
