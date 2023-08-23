a41c752fa0efe538570863fbe5d29332f99b4648
Evelyn <evelyn7d7@gmail.com>
2023-08-22 19:33:31 -0700

    100 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++d
+			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -32,9 +32,9 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0
+			f = 0;
 		}
-		i++
+		i++;
 	}
 
 	if (f == 0)
