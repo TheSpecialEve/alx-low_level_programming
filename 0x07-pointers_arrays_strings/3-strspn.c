a9471dae2887de45816c827a2c8bce5aa69b2281
TheSpecialEve <Evelyn7d7@gmail.com>
2023-08-29 14:06:08 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,38 +9,32 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int n = 0;
+	int r;
 
-		unsigned int n = 0;
-
-		int r;
-
-
-		while (*s)
-
-		{
-
+	while (*s)
+	{
 		for (r = 0; accept[r]; r++)
-
 		{
-
-		if (*s == accept[r])
-
-		{
-
-		n++;
-
-		break;
-
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
-
-		else if (accept[r + 1] == '\0')
-
-		return (n);
-
 		s++;
-
-		}
-
-		return (n);
-
+	}
+	return (n);
 }
+
+
+
+
+
+
+
+
+
+
