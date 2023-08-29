14a7a3fa06481a62f134061e99c6a8f543a8305c
TheSpecialEve <Evelyn7d7@gmail.com>
2023-08-29 14:53:58 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,18 +9,28 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
-	{
+
+		for (; *haystack != '\0'; haystack++)
+
+		{
+
 		char *l = haystack;
 		char *p = needle;
 
-		while (*l == *p && *p != '\0")
+
+		while (*l == *p && *p != '\0')
 		{
-			l++;
-			p++;
-		}
+		l++;
+		p++;
+}
+
 		if (*p == '\0')
-			return (haystack);
-	}
-	return (0);
+
+		return (haystack);
+
+		}
+
+
+return (0);
+
 }
